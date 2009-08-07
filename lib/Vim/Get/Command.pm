97c098f0ed819c685727e215b2151df54f6379b3
c9s <cornelius.howl@gmail.com>
2009-08-07 11:13:47 +0800

    add command

--- lib/Vim/Get/Command.pm
+++ lib/Vim/Get/Command.pm
@@ -2,6 +2,28 @@ package Vim::Get::Command;
 use warnings;
 use strict;
 
+use base qw(App::CLI App::CLI::Command);
+use Getopt::Long qw(:config no_ignore_case bundling);
+
+sub invoke {
+    my ($pkg , $cmd,  @args) = @_;
+    my ($ret);
+
+    local *ARGV = [$cmd, @args];
+
+    $ret = eval {
+        $pkg->dispatch();
+    };
+
+    # $logger->info( $ret) if $ret && $ret !~ /^\d+$/;
+#    if ($@ && !ref($@)) {
+#        $logger->info("$@");
+#    }    
+    # $ret = 1 if ($ret ? $ret !~ /^\d+$/ : $@); 
+    warn $@ if $@;
+
+    return ($ret || 0);
+}
 
 
 1;
