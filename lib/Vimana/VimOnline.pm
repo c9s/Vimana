9babfe20e91de80ccb1e4bbc3741e8d575a32142
c9s <cornelius.howl@gmail.com>
2009-08-08 01:23:59 +0800

    remove text-table

--- lib/Vimana/VimOnline.pm
+++ lib/Vimana/VimOnline.pm
@@ -3,27 +3,19 @@ package Vimana::VimOnline::SearchResult;
 require Vimana::VimOnline::Search;
 require Vimana::VimOnline::ScriptPage;
 
-use Text::Table;
-
 sub display {
     my ($class , $results ) = @_; 
 
-    my $tb = Text::Table->new( "name", "rating", 'type', "description" );
-
-
+    my $columns = 18;
     while( my ( $script_name , $item ) = each %$results ) {
-        $tb->add(
-            $script_name , 
-            $item->{rating},
-            $item->{type},
-            $item->{summary}->{text},
-        );
+        print $script_name;
+        print ' ' x ( $columns - length $script_name);
+        print " - " . $item->{summary}->{text} . "\n";
         # printf( "%s (%s) - %s -  %s\n" , 
         # $script_name , $item->{rating} , 
         # $item->{type} ,  $item->{summary}->{text} ); 
     }
 
-    print $tb;
 }
 
 
