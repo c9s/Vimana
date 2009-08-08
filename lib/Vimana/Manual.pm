9259f8b42d740da67533029f89eadc1994f9d45f
c9s <cornelius.howl@gmail.com>
2009-08-08 16:58:51 +0800

    add manual

---
+++ lib/Vimana/Manual.pm
@@ -0,0 +1,24 @@
+=begin
+
+=head1 NAME
+
+Vimana::Manual
+
+=head1 DESCRIPTION
+
+=head1 SYNOPSIS
+
+=head1 AUTHOR
+
+You-An Lin 林佑安 ( Cornelius / c9s ) C<< <cornelius.howl at gmail.com> >>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2007 You-An Lin 林佑安 ( Cornelius / c9s ), all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+=cut
