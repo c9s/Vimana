8c6515088165dd773911a2b36bdde4622cc23ebe
c9s <cornelius.howl@gmail.com>
2009-10-23 02:16:20 +0800

    remove search display class

--- lib/Vimana/VimOnline.pm
+++ lib/Vimana/VimOnline.pm
@@ -4,23 +4,4 @@ use strict;
 require Vimana::VimOnline::Search;
 require Vimana::VimOnline::ScriptPage;
 
-package Vimana::VimOnline::SearchResult;
-
-
-sub display {
-    my ($class , $results ) = @_; 
-
-    my $columns = 18;
-    while( my ( $script_name , $item ) = each %$results ) {
-        print $script_name;
-        print ' ' x ( $columns - length $script_name);
-        print " - " . $item->{summary}->{text} . "\n";
-        # printf( "%s (%s) - %s -  %s\n" , 
-        # $script_name , $item->{rating} , 
-        # $item->{type} ,  $item->{summary}->{text} ); 
-    }
-
-}
-
-
 1;
