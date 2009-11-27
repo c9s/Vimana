6240ae9d35b015b06e9fd83dadde1c34f3d7180d
c9s <cornelius.howl@gmail.com>
2009-11-28 02:25:21 +0800

    try to load yaml record files

--- lib/Vimana/Command/Installed.pm
+++ lib/Vimana/Command/Installed.pm
@@ -4,6 +4,7 @@ use strict;
 use base qw(App::CLI::Command);
 use Vimana::Logger;
 use Vimana::PackageFile;
+use File::Find;
 
 =head2 run
 
@@ -12,9 +13,33 @@ find installed packages.
 =cut
 
 sub run {
-
     my $self = shift;
+    my @dir = ( File::Spec->join( $ENV{HOME}  , '.vim' , 'record' ) );
+    File::Find::find( sub { 
 
+    # $File::Find::dir is the current directory name,
+    # $_ is the current filename within that directory
+    # $File::Find::name is the complete pathname to the file.
+    # 
+    # follow VIM::Packager META data
+    # 
+        my $record = YAML::LoadFile($_);
+        unless( $record ) {
+            print STDERR "ERROR: Record $_ load failed.\n";
+            return;
+        }
+        unless( $record->{meta} ) {
+            print STDERR "ERROR: Record $_ doesn't contain meta record.\n";
+            return;
+        }
+
+        unless( $record->{meta}{name} ) {
+            print STDERR "ERROR: Record $_ meta doesn't have package name.\n";
+            return;
+        }
+        print $record->{meta}{name} . "\n";
+
+    } , @dir);
 }
 
 
