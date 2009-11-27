6745bdfa627b09f8bd9c9189660b95e42bf7556c
c9s <cornelius.howl@gmail.com>
2009-11-28 01:54:55 +0800

    add installed command.

---
+++ lib/Vimana/Command/Installed.pm
@@ -0,0 +1,22 @@
+package Vimana::Command::Installed;
+use warnings;
+use strict;
+use base qw(App::CLI::Command);
+use Vimana::Logger;
+use Vimana::PackageFile;
+
+=head2 run
+
+find installed packages.
+
+=cut
+
+sub run {
+
+    my $self = shift;
+
+}
+
+
+
+1;
