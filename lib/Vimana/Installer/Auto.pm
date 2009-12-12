a847e126688982d58778acf4e6110b2fcc167b9d
c9s <cornelius.howl@gmail.com>
2009-12-12 15:18:59 +0800

    code migrate

--- lib/Vimana/Installer/Auto.pm
+++ lib/Vimana/Installer/Auto.pm
@@ -1,12 +1,213 @@
 package Vimana::Installer::Auto;
-use base qw(Vimana::Installer);
+use base qw(Vimana::Installer Vimana::Accessor);
+use base qw(Vimana::Accessor);
 use warnings;
 use strict;
-use Vimana::AutoInstall;
+
+# use re 'debug';
+use File::Copy::Recursive qw(fcopy rcopy dircopy fmove rmove dirmove);
+use File::Spec;
+use File::Path qw'mkpath rmtree';
+use Archive::Any;
+use File::Find;
+use File::Type;
+use Vimana::Logger;
+use Vimana::Util;
+use DateTime;
+
+__PACKAGE__->mk_accessors( qw(package) );
+
 sub run {
-    my ($class,$pkgfile) = @_;
-    my $auto = Vimana::AutoInstall->new( { package => $pkgfile, options => {} } );
-    return $auto->run();  # XXX: dry_run , verbose
+    my ($self,$pkgfile) = @_;
+
+    $self->package( $pkgfile );
+
+    if( $pkgfile->is_archive() ) {
+        $logger->info('Archive file');
+        return $self->install_from_archive;
+    }
+    elsif( $pkgfile->is_text() ) {
+        $logger->info('Plain Text file');
+
+        my $type = $self->inspect_text_content;
+        if ($type) {
+            $logger->info("Found script type: $type");
+            return $self->install_to($type);
+        }
+
+        return $self->install_to( 'colors' )
+            if $pkgfile->script_is('color scheme');
+
+        return $self->install_to( 'syntax' )
+            if $pkgfile->script_is('syntax');
+
+        return $self->install_to( 'indent' )
+            if $pkgfile->script_is('indent');
+
+        return $self->install_to( 'ftplugin' )
+            if $pkgfile->script_is('ftplugin');
+
+        return 0;
+    }
+
 }
+
+sub install_to {
+    my ( $self , $dir ) = @_;
+    my $file = $self->package->file;
+    my $target = File::Spec->join( runtime_path(), $dir );
+    File::Path::mkpath [ runtime_path() ];
+
+    $logger->info( "Install $file to $target" );
+    my $ret = fcopy( $file => $target );
+    !$ret ? 
+        $logger->error( $! ) :
+        $logger->info("Installed");
+    $ret;
+}
+
+
+
+sub find_vimball_files {
+    my $out = shift;
+    my @vimballs;
+    File::Find::find(  sub {
+            return unless -f $_;
+            push @vimballs , File::Spec->join($File::Find::dir , $_ ) if /\.vba$/;
+        } , $out );
+    return @vimballs;
+}
+
+sub install_from_archive {
+    my $self = shift;
+    my $pkg = $self->package;
+
+    my @files = $pkg->archive->files;
+
+    for (@files ) {
+        print "\t$_\n";
+    }
+
+    # my $out = $pkg->extract_to( );
+    my $out = Vimana::Util::tempdir();
+    rmtree [ $out ] if -e $out;
+    mkpath [ $out ];
+    $logger->info("Temporary directory created: $out") ;
+
+    $logger->info("Extracting...") ;
+    $pkg->archive->extract( $out );  
+
+    if( $pkg->has_vimball() ) {
+        $logger->info( "vimball files found, trying to install vimballs");
+        use Vimana::VimballInstall;
+        my @vimballs = find_vimball_files $out;
+        Vimana::VimballInstall->install_vimballs( @vimballs );
+    }
+
+    # check directory structure
+    {
+
+        # XXX: check vim runtime path subdirs , mv to init script
+        $logger->info("Initializing vim runtime directories") ;
+        Vimana::Util::init_vim_runtime();
+
+        my @files;
+        File::Find::find(  sub {
+                return unless -f $_;
+                push @files , File::Spec->join( $File::Find::dir , $_ ) if -f $_;
+            } , $out );
+
+        my $nodes = $self->find_base_path( \@files );
+        unless ( keys %$nodes ) {
+            $logger->warn("Can't found base path.");
+            return 0;
+        }
+        
+        $logger->info('base path:');
+        $logger->info( $_ ) for ( keys %$nodes );
+
+        $self->install_from_nodes( $nodes , runtime_path() );
+
+        $logger->info("Updating helptags");
+        $self->update_vim_doc_tags();
+    }
+
+    $logger->info("Clean up temporary directory.");
+    rmtree [ $out ] if -e $out;
+
+    return 1;
+}
+
+=head2 install_from_nodes
+
+=cut
+
+sub install_from_nodes {
+    my ($self , $nodes , $to ) = @_;
+    $logger->info("Copying files...");
+    for my $node  ( grep { $nodes->{ $_ } > 1 } keys %$nodes ) {
+        $logger->info("$node => $to");
+        my (@ret) = dircopy($node, $to );
+
+    }
+}
+
+=head2 i_know_what_to_do
+
+=cut
+
+sub i_know_what_to_do {
+    my $nodes = shift;
+    for my $v ( values %$nodes ) {
+        return 1 if $v > 1;
+    }
+    return 0;  # i am not sure
+}
+
+
+=head2 find_base_path 
+
+=cut
+
+sub find_base_path {
+    my ( $self, $paths ) = @_;
+    my $nodes = {};
+    for my $p ( @$paths ) {
+        if ( $p =~ m{^(.*?/)?(plugin|doc|syntax|indent|colors|autoload|after|ftplugin)/.*?\.(vim|txt)$} ) {
+            $nodes->{ $1 || '' } ++;
+        }
+    }
+    return $nodes;
+}
+
+sub update_vim_doc_tags {
+    my $vim = find_vim();
+    my $dir = File::Spec->join( runtime_path() , 'doc' );
+    system(qq|$vim -c ':helptags $dir'  -c q |);
+}
+
+sub inspect_text_content {
+    my $self = shift;
+    my $content = $self->package->content;
+    return 'colors'   if $content =~ m/^let\s+(g:)?colors_name\s*=/;
+    return 'syntax'   if $content =~ m/^syn[tax]* (?:match|region|keyword)/;
+    return 'compiler' if $content =~ m/^let\s+current_compiler\s*=/;
+    return 'indent'   if $content =~ m/^let\s+b:did_indent/;
+    return undef;
+}
+
+=head1 AUTHOR
+
+You-An Lin 林佑安 ( Cornelius / c9s ) C<< <cornelius.howl at gmail.com> >>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2007 You-An Lin ( Cornelius / c9s ), all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
 
 1;
