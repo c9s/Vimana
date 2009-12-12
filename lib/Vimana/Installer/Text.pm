1803e91c1eb4d7c4d7ff24a2b109efdba317c91c
c9s <cornelius.howl@gmail.com>
2009-12-12 14:08:46 +0800

    add text type installer

---
+++ lib/Vimana/Installer/Text.pm
@@ -0,0 +1,17 @@
+package Vimana::Installer::Text;
+use warnings;
+use strict;
+use base qw(Vimana::Installer);
+
+sub new { bless {}, shift; }
+
+sub run {
+    my ( $class, $pkgfile ) = @_;
+
+
+
+
+
+}
+
+1;
