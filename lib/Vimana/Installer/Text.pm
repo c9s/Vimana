130e15136b477a5da6f404b09ad69cff24aff289
c9s <cornelius.howl@gmail.com>
2009-12-14 00:40:59 +0800

    inspect text content

--- lib/Vimana/Installer/Text.pm
+++ lib/Vimana/Installer/Text.pm
@@ -31,6 +31,25 @@ sub run {
 
     # guess text filetype here.  (colorscheme, ftplugin ...etc)
 
+    my $type = $self->inspect_text_content;
+    if ($type) {
+        $logger->info("Script type found: $type");
+        $self->copy_to_rtp($type);
+        return 1;
+    }
+
 }
+
+
+sub inspect_text_content {
+    my $self = shift;
+    my $content = $self->package->content;
+    return 'colors'   if $content =~ m/^let\s+(g:)?colors_name\s*=/;
+    return 'syntax'   if $content =~ m/^syn[tax]* (?:match|region|keyword)/;
+    return 'compiler' if $content =~ m/^let\s+current_compiler\s*=/;
+    return 'indent'   if $content =~ m/^let\s+b:did_indent/;
+    return 0;
+}
+
 
 1;
