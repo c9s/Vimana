c1ebf1767a8b0fbb892b810ad6e068fa9ee53645
c9s <cornelius.howl@gmail.com>
2009-12-14 01:46:38 +0800

    try to require vim::packager if needed.

--- lib/Vimana/Installer/Meta.pm
+++ lib/Vimana/Installer/Meta.pm
@@ -3,4 +3,25 @@ use base qw(Vimana::Installer);
 use warnings;
 use strict;
 
+
+sub run {
+    my $self = shift;
+    my $path = shift;
+
+    # try to require VIM::Packager
+    eval(q|require VIM::Packager;|);
+    if( $@ ) {
+        $logger->info( "It seems you dont have VIM::Packager installed." );
+        $logger->info( "meta file can't be translated to makefile." );
+        return 0;
+    }
+
+    my $ret;
+    $ret = system( 'vim-packager build' );
+
+    $ret = system( 'make install' );
+
+
+}
+
 1;
