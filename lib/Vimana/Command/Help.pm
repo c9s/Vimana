c2c716a1b9ffd0631e9507e5d55b74d9a6602736
c9s <cornelius.howl@gmail.com>
2010-01-25 02:06:51 +0800

    add help message. (finally)

--- lib/Vimana/Command/Help.pm
+++ lib/Vimana/Command/Help.pm
@@ -5,6 +5,23 @@ use base qw(App::CLI::Command);
 
 sub run {
 
+    print <<END
+
+    Usage:
+
+        \$ vimana [command] [arguments]
+
+    Avaliable Commands:
+
+        update          - update index for searching.
+        install (i)     - install package
+        remove  (r)     - remove package
+        search  (s)     - search packages
+        help            - show this help
+
+    
+END
+
 }
 
 1;
