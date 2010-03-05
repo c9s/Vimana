0d4073059e0fb9b86725d08c7a5041fae7b6edec
c9s <cornelius.howl@gmail.com>
2010-03-06 00:46:25 +0800

    update help message.

--- lib/Vimana/Command/Help.pm
+++ lib/Vimana/Command/Help.pm
@@ -16,32 +16,42 @@ sub run {
 
         print <<END;
 
-        Usage:
+Usage:
 
-            \$ vimana [command] [arguments]
+    \$ vimana [command] [arguments]
 
-        Avaliable Commands:
+Avaliable Commands:
 
-            update          - update index for searching.
+    update          - update index for searching.
 
-            install (i)     - install package
+    install (i)     - install package
 
-                install [plugin]
+        install [plugin]
 
-            remove  (r)     - remove package
-                
-                remove [plugin] 
+        -r,--runtime-path [path]      install to [path] runtime path.
+                                        you might need to add 'runtimepath' option 
+                                        in your .vimrc file.
 
-            search  (s)     - search packages
+        -v,--verbose                  verbose
 
-                search [keyword]
+        -y,--yes                      assume yes
 
-            help            - show this help
+        -f,--force                    force install
 
-                help [command]
-                help [topic]
+    remove  (r)     - remove package
+        
+        remove [plugin] 
 
-        Help Topics:
+    search  (s)     - search packages
+
+        search [keyword]
+
+    help            - show this help
+
+        help [command]
+        help [topic]
+
+Help Topics:
 
 END
     }
