b4f480afebd5ec438e5195b0babd5553fcbd0847
c9s <cornelius.howl@gmail.com>
2010-03-07 16:54:22 +0800

    Fix rakefile installer.

--- lib/Vimana/Installer/Rakefile.pm
+++ lib/Vimana/Installer/Rakefile.pm
@@ -5,8 +5,8 @@ use strict;
 
 # XXX: check if we have rake command installed.
 sub run {
-    my ($self,$pkgfile,$path)=@_;
-    if ( -e "rakefile" or -e 'rakefile' ) {
+    my ($self)=@_;
+    if ( -e "rakefile" or -e 'Rakefile' ) {
         print "Rakefile found. do rake install.\n";
         my $ret = system( "rake install" );
         return 1 if $ret == 0;
