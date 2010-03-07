df71f071cd850bb9c620a2e8e25e69f6d7750f99
c9s <cornelius.howl@gmail.com>
2010-03-08 00:28:56 +0800

    improve Upgrade command.

--- lib/Vimana/Command/Upgrade.pm
+++ lib/Vimana/Command/Upgrade.pm
@@ -1,17 +1,20 @@
 package Vimana::Command::Upgrade;
 use warnings;
 use strict;
+use base qw(App::CLI::Command);
+use Vimana::Record;
+use Vimana::Installer;
 
 
 # XXX: implement this
 sub run {
-    my ($self,$cname) = @_;
+    my ($self,$name) = @_;
 
     # find installation record.
     # remove old install
     # install new one.
-
-
+    Vimana::Record->remove( $name );
+    Vimana::Installer->install( $name );
 }
 
 
