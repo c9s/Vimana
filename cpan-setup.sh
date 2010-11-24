1736e5f191f59252417a8fed637e085a1f7ea95d
c9s <cornelius.howl@gmail.com>
2010-11-24 14:26:15 +0800

    Checking in changes prior to tagging of version 2.1.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 4daed6c..9d2b6e0 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,18 +1,4 @@
     Revision history for Vimana
    
    -0.1
    -        * support install record
    -
    -0.075
    -        * remove Moose dependency
    -
    -0.07
    -        * supports Vimball install.
    -        * provide download command.
    -
    -
    -0.06
    -        supports auto-install (smart install) for packages
    -
    -0.01
    -        first version, released on an unsuspecting world.
    +2.1
    +        * Remove meta strategy.

--- cpan-setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-MIRROR=http://cpan.nctu.edu.tw/
-echo "" | perl -MCPAN -e 'mkmyconfig'
-echo "o conf prerequisites_policy follow " | cpan
-echo "o conf urllist unshift $MIRROR" | cpan
-echo "Done"
--- lib/Vimana.pm
+++ lib/Vimana.pm
@@ -4,7 +4,7 @@ use strict;
 use Vimana::Index;
 use vars qw($INDEX);
 
-our $VERSION = 2010.09303;
+our $VERSION = '2.1';
 
 sub index {
     return $INDEX if $INDEX;
