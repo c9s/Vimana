56f4db4c91120e30e1064728a41f7ea8f454b6da
c9s <cornelius.howl@gmail.com>
2011-11-06 13:22:08 +0800

    Clean up

--- dist_par_binary.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-pp -P -cd _deps_cache -I lib -o vimana-macosx bin/vimana 
-./vimana-macosx search rail
-./vimana-macosx update
-./vimana-macosx install snipmate
-read -p 'SCP?'
-scp vimana-macosx oulixe.us:/var/www/vimana-macosx
